8b5011166ddc3617d7a7a6bb1e860555a6d06dfb
Jeffrey <1115378579@qq.com>
2022-08-13 17:57:28 +0800

    feat：添加config

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+export default {
+  port: 3100,
+  dburl: '',
+  mysqlConfig: {
+    database: "database",
+    username: "admin",
+    password: "ps.",
+    host: "xxx",
+    port: 21729,
+  },
+}
